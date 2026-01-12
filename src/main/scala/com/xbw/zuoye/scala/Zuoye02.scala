1041c32adb9d2714a3d26ca530d41fc60886b0c4
hid55257409 <manhtuyetbac84008@gmail.com>
2026-01-12 20:28:20 +0800

    0.1版本

--- src/main/scala/com/xbw/zuoye/scala/Zuoye02.scala
+++ src/main/scala/com/xbw/zuoye/scala/Zuoye02.scala
@@ -1,5 +1,6 @@
 package com.xbw.zuoye.scala
 
+import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 object Zuoye02 {
@@ -9,24 +10,23 @@ object Zuoye02 {
 */
 def main(args: Array[String]): Unit = {
   // 学生分数表
-  val ScoreList = Source.fromFile("./data/score.csv")
+  val SourceSco = Source.fromFile("./data/score.csv")
+  val ScoreList = SourceSco.getLines().toList
   // 学生信息表
-  val StuList= Source.fromFile("./data/students.csv")
+  val SourceStu=Source.fromFile("./data/students.csv")
+  val StuList= SourceStu.getLines().toList
 
+  println("*"*30)
   // 1.统计性别人数 [性别,人数]
   StuList
-    .getLines()
-    .toList
     .map(student=>student.split(',')(3))
     .groupBy(student=>student)
     .map(gender=>s"${gender._1},${gender._2.size}")
     .foreach(println)
 
-
+  println("*"*30)
   // 2. 统计每个学生平均分 [学号,平均分]
   ScoreList
-    .getLines()
-    .toList
     .map(Score => {
       val SplitList = Score.split(",")
       val Split_1 = SplitList(0)
@@ -44,14 +44,35 @@ def main(args: Array[String]): Unit = {
 
 
   // 3. 统计每个学生的总分 [学号,学生姓名,学生年龄,总分]
+  println("*"*30)
+  var ScoList = ScoreList
+    .map(Score => {
+      val SplitList = Score.split(",")
+      val Split_1 = SplitList(0)
+      val Split_2 = SplitList(2).toDouble
+      (Split_1,Split_2)
+    })
+    .groupBy(score=>score._1)
+    .map(score=>{
+      val id = score._1
+      val Score = score._2.map(ss=>ss._2)
+      val sum = Score.sum
+      (id,sum)
+    })
 
   StuList
-    .getLines()
-    .toList
+    .map(stu=>{
+      val Splits = stu.split(",")
+      val id = Splits(0)
+      val name = Splits(1)
+      val gender = Splits(2)
+      val sumScore = ScoList.getOrElse(id,0)
+      (id,name,gender,sumScore)
+    }).foreach(println)
 
 
-  ScoreList.close()
-  StuList.close()
+  SourceSco.close()
+  SourceStu.close()
 
   }
 }
