b0093659c54a6a6fd1a1fb41d121cad8b93d139f
hid55257409 <manhtuyetbac84008@gmail.com>
2026-01-23 20:33:06 +0800

    0.1版本

--- Spark/src/main/scala/com/sql/Demo01SS.scala
+++ Spark/src/main/scala/com/sql/Demo01SS.scala
@@ -1,9 +1,62 @@
 package com.sql
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Demo01SS {
   def main(args: Array[String]): Unit = {
 
+    // 建立SparkSeesion对象
+    val spark: SparkSession = SparkSession.builder()
+      .appName(this.getClass.getSimpleName.replace("$", ""))
+      .master("local")
+      .getOrCreate()
+
+    // 完成单词统计
+    // 第一种方法:使用SparkContext方法(RDD)
+
+    val sc: SparkContext = spark.sparkContext
+    sc.textFile("./data/Spark/word.txt")
+      .flatMap((word: String) =>word.split(","))
+      .groupBy(word=>word)
+      .map(word=>word._2.size)
+      .foreach(println)
+
+    // 第二种方法:使用SparkSQL方法
+    val LineSQL: DataFrame = spark.read
+      // 表示读取csv的文件
+      .format("csv")
+      // 表示使用的分隔符号是|
+      .option("sep", "|")
+      // 表示给列名称为line
+      .schema("line String")
+      // 表示读取./data/Spark/word.txt
+      .load("./data/Spark/word.txt")
+
+    // 创建视图,视图名称为words
+    LineSQL.createTempView("words")
+
+    // 编写SQL
+    spark.sql(
+      """
+        |select t1.word
+        |       ,count(*) as cnt
+        |from (
+        |     select explode(split(line,",")) as word
+        |     from words
+        |) t1 group by t1.word
+        |""".stripMargin).show(5)
+
+    // 第三种方式DSL
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
+
+    LineSQL.select(explode(split($"line",",")) as "word")
+      .groupBy($"word")
+      .agg(count("*") as "cnt")
+      .show()
+
+
   }
 }
+
--- Spark/src/main/scala/com/sql/Demo02Source.scala
+++ Spark/src/main/scala/com/sql/Demo02Source.scala
@@ -18,13 +18,42 @@ object Demo02Source {
       .schema("id String,name String,age Int,gender String,clazz String")
       .load("./data/Spark/students.csv")
     // 默认打印前20行
-    SpCSV.show()
+    //    SpCSV.show()
 
     //    SpCSV.write.format("json").mode(SaveMode.Overwrite).save("./data/Spark/01_json")
     //    SpCSV.write.format("orc").mode(SaveMode.Overwrite).save("./data/Spark/02_orc")
     //    SpCSV.write.format("parquet").mode(SaveMode.Overwrite).save("./data/Spark/03_parquet")
 
+    // json格式文件的读取
+    spark.read
+      .format("json")
+      .option("sep", "|")
+      .load("./data/Spark/01_json")
+    //      .show()
 
+    // orc格式文件的读取
+    spark.read
+      .format("orc")
+      .option("sep", "|")
+      .load("./data/Spark/02_orc")
+    //      .show()
+
+    // parquet格式文件的读取
+    spark.read
+      .format("parquet")
+      .option("sep", "|")
+      .load("./data/Spark/03_parquet")
+    //      .show()
+
+    // JDBC
+    spark.read
+      .format("jdbc")
+      .option("url", "jdbc:mysql://localhost:3306/wb")
+      .option("dbtable", "comments")
+      .option("user", "root")
+      .option("password", "123456")
+      .load()
+      .show()
 
   }
 }
